807fbdcf908e05aa57f5c1a657078810f99d6ab5
Lucas Noki <lucas-noki@gmx.de>
2020-12-19 17:55:17 +0100

    searchndestroy.sh filter duplicate entries

--- searchndestroy.sh
+++ searchndestroy.sh
@@ -3,4 +3,4 @@ host=$2
 outfile=$3
 
 cat $domainlist | sed 's~http[s]*://~~g' | grep $host | rev | cut -d. -f 1,2,3 | rev | sort -u \
-| xargs -n 1 -I{} assetfinder --subs-only {} | httprobe | tee $outfile
+| xargs -n 1 -I{} assetfinder --subs-only {} | httprobe | sort -u | tee $outfile

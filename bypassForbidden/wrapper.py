aa7ca707f0b1167ae8bb2dbe27f9ec87143b0829
zPrototype <43825360+zPrototype@users.noreply.github.com>
2021-02-09 00:23:03 +0100

    Update wrapper.py
    bla

--- bypassForbidden/wrapper.py
+++ bypassForbidden/wrapper.py
@@ -3,7 +3,6 @@ import argparse
 import re
 import json
 from concurrent.futures import ThreadPoolExecutor
-from rich.progress import Progress, TimeRemainingColumn, BarColumn
 from rich.console import Console
 
 parser = argparse.ArgumentParser()
@@ -12,6 +11,8 @@ parser.add_argument("--urls", "-u", help="Enter a file of URLs you want to test"
 parser.add_argument("--script", "-s", help="Enter the path for the script", required=True)
 parser.add_argument("--output", "-o", help="Enter the name for an output file")
 args = parser.parse_args()
+
+CONSOLE = Console()
 
 with open(args.urls) as handle:
     urls = handle.readlines()
@@ -31,19 +32,12 @@ def execute_script(url, script):
     return list(output)
 
 
+CONSOLE.print("[bold cyan]Trying bypasses...")
 with ThreadPoolExecutor(max_workers=args.threads) as executor:
     for url in urls:
         results.append((url, executor.submit(execute_script, url, args.script)))
     
-    with Progress("[progress.description]{task.description}", BarColumn(),
-                      "[progress.percentage]{task.percentage:>3.0f}%", TimeRemainingColumn(),
-                      "[deep_sky_blue1]{task.completed} of {task.total}") as progress:
-            my_task = progress.add_task("[green]Trying bypasses...", total=len(results))
-            count_remaining = len(list(filter(lambda f: not f[1].done(), results)))
-            while count_remaining > 0:
-                count_remaining = len(list(filter(lambda f: not f[1].done(), results)))
-                progress.update(my_task, completed=len(results) - count_remaining)
-                time.sleep(1)
+    
 
 results = map(lambda r: (r[0], r[1].result()), results)
 results = list(filter(lambda x: x[1], results))

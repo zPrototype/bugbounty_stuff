a599bc9625cfed94fcd698ae4c6122f98d9b9985
Lucas Noki <lucas-noki@gmx.de>
2021-02-20 04:19:27 +0100

    Fix the rich progressbar

--- statusCodeGetter.py
+++ statusCodeGetter.py
@@ -2,14 +2,13 @@ import requests
 import argparse
 from urllib3.exceptions import InsecureRequestWarning
 from concurrent.futures import ThreadPoolExecutor
-from rich.console import Console
+from rich.progress import Progress, BarColumn, TimeRemainingColumn
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", help="Enter a file with URLs to check", required=True)
 parser.add_argument("--output", "-o", help="Enter the name of a output file", required=True)
 args = parser.parse_args()
 
-CONSOLE = Console()
 results = []
 
 with open(args.file) as handle:
@@ -25,11 +24,21 @@ def make_request(url):
         res = False
     return res
 
+
 with ThreadPoolExecutor(max_workers=10) as executor:
-    with CONSOLE.status("") as status:
-        for i, url in enumerate(urls):
-            status.update(f"[cyan]Working on statuscodes... ({i}/{len(urls)})")
-            results.append((url, executor.submit(make_request, url)))
+    for i, url in enumerate(urls):
+        results.append((url, executor.submit(make_request, url)))
+
+    # Progress bar
+    with Progress("[progress.description]{task.description}", BarColumn(),
+                  "[progress.percentage]{task.percentage:>3.0f}%", TimeRemainingColumn(),
+                  "[deep_sky_blue1]{task.completed} of {task.total}") as progress:
+        my_task = progress.add_task("[green]Dorking...", total=len(results))
+        count_remaining = len(list(filter(lambda f: not f[1].done(), results)))
+        while count_remaining > 0:
+            count_remaining = len(list(filter(lambda f: not f[1].done(), results)))
+            progress.update(my_task, completed=len(results) - count_remaining)
+            time.sleep(1)
 
 results = map(lambda r: (r[0], r[1].result()), results)
 results = list(filter(lambda x: x[1], results))

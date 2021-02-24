b0a395c6984a33a82170cc8df716c699cb7aefbf
Lucas Noki <lucas-noki@gmx.de>
2021-02-24 18:09:52 +0100

    Remove protocol from sublist3r output

--- enum_subs.sh
+++ enum_subs.sh
@@ -6,8 +6,9 @@ findomain -t $target -u ./$target.fd
 assetfinder --subs-only $target | tee $target.af
 sublist3r -d $target -o ./$target.sl
 
+remove-http $target.sl
 cat $target.fd $target.af $target.sl | sort -u | tee unprobed.out
-cat unprobed.out | httprobe | tee master.txt
+cat unprobed.out | httprobe | tee $1.txt
 rm $target.fd $target.af $target.sl unprobed.out
 
 echo ""

ab4468a8952e8119db015e1a3b8473514a1694e4
Lucas Noki <lucas-noki@gmx.de>
2022-01-15 19:13:52 +0100

    Add Dockerfile and deploy script

---
+++ subkiller/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Ignore this file. This is only for updating the docker build on dockerhub.
+sudo docker buildx build --push --platform linux/amd64,linux/arm64/v8 --tag 0xprototype/subkiller:20220115 --tag 0xprototype/subkiller:v1 --tag 0xprototype/subkiller:latest .

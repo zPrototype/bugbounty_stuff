e984cf7e49110f41f27ca279d930ae2a10e99d0c
Lucas Noki <mail@lucasnoki.com>
2022-06-11 18:45:52 +0200

    Add type to argument, fix default value, add progress display

--- bypassForbidden/wrapper.py
+++ bypassForbidden/wrapper.py
@@ -4,9 +4,10 @@ import re
 import json
 from concurrent.futures import ThreadPoolExecutor
 from rich.console import Console
+from rich.progress import track
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--threads", "-t", help="Enter the number of threads you want to use", default=True)
+parser.add_argument("--threads", "-t", help="Enter the number of threads you want to use", default=5, type=int)
 parser.add_argument("--urls", "-u", help="Enter a file of URLs you want to test", required=True)
 parser.add_argument("--script", "-s", help="Enter the path for the script", required=True)
 parser.add_argument("--output", "-o", help="Enter the name for an output file")
@@ -22,8 +23,8 @@ results = []
 filter_400_codes = re.compile(r"4\d+")
 
 
-def execute_script(url, script):
-    command = f"{script} -r -c {url}"
+def execute_script(forbidden_url, script):
+    command = f"{script} -r -c {forbidden_url}"
     output = subprocess.check_output(f"{command}", shell=True, stderr=subprocess.PIPE)
     output = output.decode("UTF-8").splitlines()
     output = filter(lambda x: not x.startswith("[+]"), output)
@@ -34,10 +35,8 @@ def execute_script(url, script):
 
 CONSOLE.print("[bold cyan]Trying bypasses...")
 with ThreadPoolExecutor(max_workers=args.threads) as executor:
-    for url in urls:
+    for url in track(urls, total=len(urls), description="Working..."):
         results.append((url, executor.submit(execute_script, url, args.script)))
-    
-    
 
 results = map(lambda r: (r[0], r[1].result()), results)
 results = list(filter(lambda x: x[1], results))
